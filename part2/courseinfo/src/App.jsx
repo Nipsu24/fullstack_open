6e2824935205fc798567badeb596628f8fd30492
Marius Meier <mmeier@student.hive.fi>
2025-04-15 15:04:10 +0300

    refactors code in order to allow arbitrary amount of courses ex2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,48 +1,75 @@
 const Course = (props) => {
-	const total = props.course.parts.reduce((sum, part) => sum + part.exercises, 0);
-	console.log(total);
-	
 	return (
-		<div>
-			<h1>{props.course.name}</h1>
-			{props.course.parts.map(part =>
-				<p key={part.id}>
-				{part.name} {part.exercises}
-				</p>
-			)}
-			<b>total of {total} exercises</b>
-		</div>
-	)
-}
+		<>
+			{props.course.map((course) => {
+				const total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+				return (
+					<div key={course.id}>
+						<h2>{course.name}</h2>
+						{course.parts.map((part) => (
+							<p key={part.id}>
+								{part.name} {part.exercises}
+							</p>
+						))}
+						<b>total of {total} exercises</b>
+					</div>
+				);
+			})}
+		</>
+	);
+};
 
 const App = () => {
-	const course = {
-		id: 1,
-		name: 'Half Stack application development',
-		parts: [
-			{
-				name: 'Fundamentals of React',
-				exercises: 10,
-				id: 1
-			},
-			{
-				name: 'Using props to pass data',
-				exercises: 7,
-				id: 2
-			},
-			{
-				name: 'State of a component',
-				exercises: 14,
-				id: 3
-			},
-			{
-				name: 'Redux',
-				exercises: 11,
-				id: 4
-			}
-		]
-	}
-	return <Course course={course} />
+	const course = [
+		{
+			id: 1,
+			name: 'Half Stack application development',
+			parts: [
+				{
+					name: 'Fundamentals of React',
+					exercises: 10,
+					id: 1
+				},
+				{
+					name: 'Using props to pass data',
+					exercises: 7,
+					id: 2
+				},
+				{
+					name: 'State of a component',
+					exercises: 14,
+					id: 3
+				},
+				{
+					name: 'Redux',
+					exercises: 11,
+					id: 4
+				}
+			]
+		},
+		{
+			id: 2,
+			name: 'Node.js',
+			parts: [
+				{
+					name: 'Routing',
+					exercises: 3,
+					id: 1
+				},
+				{
+					name: 'Middlewares',
+					exercises: 7,
+					id: 2
+				}
+			]
+		}
+	]
+	return (
+		<>
+			<h1> Web development curriculum</h1>
+			<Course course={course} /> 
+		</>
+	)
 }
 
 export default App
