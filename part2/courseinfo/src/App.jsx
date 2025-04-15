54040d59a8b0cb8f51e8f3d7a638f77a27743632
Marius Meier <mmeier@student.hive.fi>
2025-04-15 15:11:01 +0300

    extracts course component as separate module for import

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,23 +1,4 @@
-const Course = (props) => {
-	return (
-		<>
-			{props.course.map((course) => {
-				const total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-				return (
-					<div key={course.id}>
-						<h2>{course.name}</h2>
-						{course.parts.map((part) => (
-							<p key={part.id}>
-								{part.name} {part.exercises}
-							</p>
-						))}
-						<b>total of {total} exercises</b>
-					</div>
-				);
-			})}
-		</>
-	);
-};
+import Course from './components/Course'
 
 const App = () => {
 	const course = [
