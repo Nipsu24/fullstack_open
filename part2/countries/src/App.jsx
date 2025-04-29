898a610b956602e8216c1fc315b761b6d74e1c66
Marius Meier <mmeier@student.hive.fi>
2025-04-29 15:21:46 +0300

    adds ex 2.19

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,50 +1,85 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const CountryDetails = ({ country }) => (
+	<div key={country.name.common}>
+		<h1>{country.name.common}</h1>
+		<div>Capital: {country.capital}</div>
+		<div>Area: {country.area}</div>
+		<h1>Languages</h1>
+		<ul>
+			{Object.values(country.languages).map((language) => (
+				<li key={language}>{language}</li>
+			))}
+		</ul>
+		<img src={country.flags.png} alt={`Flag of ${country.name.common}. ${country.flags.alt}`} />
+	</div>
+)
+
+const CountryList = ({ countries, setSelectedCountry }) => (
+	<div>
+		{countries.map((country) => (
+			<div key={country.name.common}>{country.name.common}
+				<button onClick={() => setSelectedCountry(country)}> Show </button>
+			</div>
+		))}
+	</div>
+)
+
+const TooManyMatches = () => (
+	<div>Too many matches, specify another filter</div>
+)
 
 const App = () => {
 	const [entry, setEntry] = useState('')
 	const [countries, setCountries] = useState([])
+	const [selectedCountry, setSelectedCountry] = useState(null)
 
-useEffect(() => {
-		console.log('effect run, entry is now', entry)
-		console.log('fetching data..')
-		axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
-		.then(response => {
-			console.log('Fetched countries:', response.data)
-			setCountries(response.data)
-		})
-		
-}, [])
+	useEffect(() => {
+			console.log('effect run, entry is now', entry)
+			console.log('fetching data..')
+			axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+			.then(response => {
+				console.log('Fetched countries:', response.data)
+				setCountries(response.data)
+			})
+			
+	}, [])
 
 	const handleChange = (event) => {
 		event.preventDefault()
 		setEntry(event.target.value)
 		console.log(event.target.value)
+		setSelectedCountry(null);
 	}
 
-const filteredCountries = countries.filter(country =>
-	country.name.common.toLowerCase().includes(entry.toLowerCase())
-)
+	const filteredCountries = countries.filter(country =>
+		country.name.common.toLowerCase().includes(entry.toLowerCase())
+	)
 
-if (filteredCountries.length > 10) {
+	const renderContent = () => {
+		if (selectedCountry) {
+			return <CountryDetails country={selectedCountry} />
+		}
+		else if (filteredCountries.length > 10) {
+			return <TooManyMatches />;
+		} 
+		else if (filteredCountries.length === 1) {
+			return <CountryDetails country={filteredCountries[0]} />
+		} 
+		else {
+			return <CountryList countries={filteredCountries} setSelectedCountry={setSelectedCountry}/>
+		}
+	};
 
-}
 	return (
 		<>
-		<form>
-			find countries <input value={entry} onChange={handleChange}/>
-		</form>
-		<div>
-			{filteredCountries.length > 10 ? (<div>Too many matches, specify another filter</div>) : (
-			filteredCountries.map((country) => (
-				<div key={country.name.common}>{country.name.common} </div>
-			))
-		)}
-
-		</div>
-		</>
-	)
+			<form>
+				find countries <input value={entry} onChange={handleChange}/>
+			</form>
+			<div>{renderContent()}</div>
+    	</>
+  )
 }
 
 export default App
