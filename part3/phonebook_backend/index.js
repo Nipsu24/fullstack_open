71b8192c95087622ccc496cc531af7af83ba7256
Marius Meier <mmeier@student.hive.fi>
2025-05-06 16:23:26 +0300

    adds ex. 3.13 and 3.14

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
+const Person = require('./models/person')
 app.use(express.json())
 
 const cors = require('cors')
@@ -12,56 +13,32 @@ app.use(express.static('dist'))
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
   app.get('/api/persons', (request, response) => {
-	response.json(persons)
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
   })
 
   app.get('/api/persons/info', (request, response) => {
-	const personsAmount = persons.length
-	const currentTime = new Date()
-	const message = `Phonebook has info for ${personsAmount} people.<br><br>${currentTime}`
-	response.send(message)
+	Person.countDocuments({}).then(count => {
+		const currentTime = new Date()
+		const message = `Phonebook has info for ${count} people.<br><br>${currentTime}`
+		response.send(message)
+	})
   })
 
   app.get('/api/persons/:id', (request, response) => {
-	const id = request.params.id
-	const person = persons.find(person => person.id === id)
-	if (person) {
+	note.findById(request.params.id).then(person => {
 		response.json(person)
-	  } else {
-		response.status(404).end()
-	  }
+	})
   })
 
-  app.delete('/api/persons/:id', (request, response) => {
-	const id = request.params.id
-	persons = persons.filter(person => person.id !== id)
+//   app.delete('/api/persons/:id', (request, response) => {
+// 	const id = request.params.id
+// 	persons = persons.filter(person => person.id !== id)
   
-	response.status(204).end()
-  })
+// 	response.status(204).end()
+//   })
 
   const generateId = () => {
 	const randNbr = Math.round(Math.random(1000) * 1000)
@@ -76,24 +53,32 @@ let persons = [
 		error: 'name or number missing' 
 	  })
 	}
-	else if (persons.find(person => person.name === body.name)) {
-		return response.status(400).json({ 
-			error: 'name must be unique' 
-		  })
-	}
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	})
+
+	person.save().then(savedPerson => {
+		response.json(savedPerson)
+	})
+	// else if (persons.find(person => person.name === body.name)) {
+	// 	return response.status(400).json({ 
+	// 		error: 'name must be unique' 
+	// 	  })
+	// }
 	
-	const person = {
-	  name: body.name,
-	  number: body.number,
-	  id: generateId(),
-	}
+	// const person = {
+	//   name: body.name,
+	//   number: body.number,
+	//   id: generateId(),
+	// }
   
-	persons = persons.concat(person)
+	// persons = persons.concat(person)
   
-	response.json(person)
+	// response.json(person)
   })
   
-  const PORT = 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
   })
--- tests/notes_backend/index.js
+++ tests/notes_backend/index.js
@@ -1,47 +1,35 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-const PORT = process.env.PORT || 3001
 
 app.use(cors())
 
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-	{
-	  id: "1",
-	  content: "HTML is easy",
-	  important: true
-	},
-	{
-	  id: "2",
-	  content: "Browser can execute only JavaScript",
-	  important: false
-	},
-	{
-	  id: "3",
-	  content: "GET and POST are the most important methods of HTTP protocol",
-	  important: true
-	}
-  ]
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+let notes = []
 
   app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>')
   })
 
   app.get('/api/notes', (request, response) => {
-	response.json(notes)
+	Note.find({}).then(notes => {
+		response.json(notes)
+	})
   })
 
   app.get('/api/notes/:id', (request, response) => {
-	const id = request.params.id
-	const note = notes.find(note => note.id === id)
-	if (note) {
-		response.json(note)
-	  } else {
-		response.status(404).end()
-	  }
+	Note.findById(request.params.id).then(note => {
+	  response.json(note)
+	})
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -51,33 +39,30 @@ let notes = [
 	response.status(204).end()
   })
 
-  const generateId = () => {
-	const maxId = notes.length > 0
-	  ? Math.max(...notes.map(n => Number(n.id)))
-	  : 0
-	return String(maxId + 1)
-  }
-  
   app.post('/api/notes', (request, response) => {
 	const body = request.body
   
 	if (!body.content) {
-	  return response.status(400).json({ 
-		error: 'content missing' 
-	  })
+	  return response.status(400).json({ error: 'content missing' })
 	}
   
-	const note = {
+	const note = new Note({
 	  content: body.content,
 	  important: body.important || false,
-	  id: generateId(),
-	}
+	})
   
-	notes = notes.concat(note)
-  
-	response.json(note)
+	note.save().then(savedNote => {
+	  response.json(savedNote)
+	})
   })
+
+  const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+  }
   
+  app.use(unknownEndpoint)
+  
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
   })
