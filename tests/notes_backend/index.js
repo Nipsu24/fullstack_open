1027780ce28d6aa89bfe07b084fea712629e3e70
Marius Meier <mmeier@student.hive.fi>
2025-05-07 15:17:04 +0300

    updates error handling

--- tests/notes_backend/index.js
+++ tests/notes_backend/index.js
@@ -7,61 +7,99 @@ const cors = require('cors')
 
 app.use(cors())
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+	
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	} 
+	
+	next(error)
+}
+
 let notes = []
 
-  app.get('/', (request, response) => {
+app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>')
-  })
+})
 
-  app.get('/api/notes', (request, response) => {
+app.get('/api/notes', (request, response) => {
 	Note.find({}).then(notes => {
 		response.json(notes)
 	})
-  })
+})
 
-  app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
 	Note.findById(request.params.id).then(note => {
-	  response.json(note)
+		if (note) {
+			response.json(note)
+		}
+		else {
+			response.status(404).end()
+		}
 	})
-  })
+	.catch(error => next(error))
+})
 
-  app.delete('/api/notes/:id', (request, response) => {
-	const id = request.params.id
-	notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+	const { content, important } = request.body
   
-	response.status(204).end()
+	Note.findById(request.params.id)
+	  .then(note => {
+		if (!note) {
+		  return response.status(404).end()
+		}
+  
+		note.content = content
+		note.important = important
+  
+		return note.save().then((updatedNote) => {
+		  response.json(updatedNote)
+		})
+	  })
+	  .catch(error => next(error))
   })
 
-  app.post('/api/notes', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
+	Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response) => {
 	const body = request.body
-  
+	
 	if (!body.content) {
-	  return response.status(400).json({ error: 'content missing' })
+		return response.status(400).json({ error: 'content missing' })
 	}
-  
+	
 	const note = new Note({
-	  content: body.content,
-	  important: body.important || false,
+		content: body.content,
+		important: body.important || false,
 	})
-  
+	
 	note.save().then(savedNote => {
-	  response.json(savedNote)
+		response.json(savedNote)
 	})
-  })
+})
 
-  const unknownEndpoint = (request, response) => {
+const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(unknownEndpoint)
-  
+}
+
+app.use(unknownEndpoint)
+
+app.use(errorHandler)
+
   const PORT = process.env.PORT
   app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
