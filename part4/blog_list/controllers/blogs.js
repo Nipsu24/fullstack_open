7cee769bc6b76ee6300710dc0d36d6fbd63353db
Marius Meier <marius.meier24@gmail.com>
2025-11-11 17:41:26 +0200

    adds ex 4.11 - 4.14

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,4 +1,6 @@
 const blogsRouter = require('express').Router()
+const { response } = require('../app')
+const blog = require('../models/blog')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -7,10 +9,34 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  if (!Object.hasOwn(body, 'title') || !Object.hasOwn(body, 'url'))
+    return response.status(400).json({error: "Missing fields in POST request"})
+  
   const blog = new Blog(request.body)
-
- const savedBlog = await blog.save()
- response.status(201).json(savedBlog)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const blogToDelete = await Blog.findByIdAndDelete(request.params.id)
+  if (blogToDelete)
+    return response.status(204).end()
+  else
+    return response.status(404).json({error: "Blog could not be found"})
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { likes } = request.body
+  const blogToUpdate = await Blog.findById(request.params.id)
+  if (blogToUpdate) {
+    blogToUpdate.likes = likes
+    const savedBlog = await blogToUpdate.save()
+    return response.status(200).json(savedBlog)
+  }
+  else
+    return response.status(404).json( {error: "Blog could not be found"})
+})
+
 
 module.exports = blogsRouter
