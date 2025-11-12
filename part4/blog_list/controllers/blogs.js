474a640994c2db78c12b9964f72be72170e17623
Marius Meier <marius.meier24@gmail.com>
2025-11-12 17:43:19 +0200

    adds ex 4.17

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,20 +1,35 @@
 const blogsRouter = require('express').Router()
 const { response } = require('../app')
-const blog = require('../models/blog')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.status(200).json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  
+  const user = await User.findById(body.userId)
+  
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
   if (!Object.hasOwn(body, 'title') || !Object.hasOwn(body, 'url'))
     return response.status(400).json({error: "Missing fields in POST request"})
   
-  const blog = new Blog(request.body)
+   const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+  
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
