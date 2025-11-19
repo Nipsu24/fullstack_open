ec792c032e47fbdd11387cd7b5b4a13fc7f2f025
Marius Meier <marius.meier24@gmail.com>
2025-11-19 11:39:40 +0200

    adds ex. 5.14 + 5.15

--- part5/frontend/src/components/Blog.test.jsx
+++ part5/frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders blog summary', () => {
@@ -12,7 +13,6 @@ test('renders blog summary', () => {
     url: 'fake-url',
     likes: '400',
   }
-
 
   const mockHandler = () => {}
 
@@ -32,4 +32,68 @@ test('renders blog summary', () => {
   const detailDiv = container.querySelector('.blogDetail')
   expect(detailDiv).not.toBeVisible()
 
+})
+
+test('clicking view button shows blog details', async () => {
+  const mockUser = {
+    username: 'testuser'
+  }
+
+  const blog = {
+    title: 'Great ways to test react components',
+    author: 'me',
+    url: 'fake-url',
+    likes: '400',
+  }
+
+  const mockHandler = () => {}
+
+  const { container } = render(
+    <Blog
+      blog={blog}
+      user={mockUser}
+      handleBlogUpdate={mockHandler}
+      handleDeleteBlog={mockHandler}
+    />
+  )
+
+  const testUser = userEvent.setup()
+  const button = screen.getByText('view')
+  await testUser.click(button)
+
+  const detailDiv = container.querySelector('.blogDetail')
+  expect(detailDiv).toBeVisible()
+  expect(detailDiv).toHaveTextContent('fake-url')
+  expect(detailDiv).toHaveTextContent('400')
+})
+
+test('clicking the button twice calls event handler twice', async () => {
+  const mockUser = {
+    username: 'testuser'
+  }
+
+  const blog = {
+    title: 'Great ways to test react components',
+    author: 'me',
+    url: 'fake-url',
+    likes: '400',
+  }
+
+  const mockHandler = vi.fn()
+
+  const { container } = render(
+    <Blog
+      blog={blog}
+      user={mockUser}
+      handleBlogUpdate={mockHandler}
+      handleDeleteBlog={mockHandler}
+    />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('like')
+  await user.click(button)
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
