a102cd3f15a3af0cc93127f7fccae959134c80b4
Marius Meier <marius.meier24@gmail.com>
2025-11-25 14:19:40 +0200

    adds ex 5.21

--- part5/backend/utils/middleware.js
+++ part5/backend/utils/middleware.js
@@ -27,7 +27,7 @@ const errorHandler = (error, request, response, next) => {
   ) {
     return response
       .status(400)
-      .json({ error: 'expeceted username to be unique' })
+      .json({ error: 'expected username to be unique' })
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
@@ -48,7 +48,7 @@ const tokenExtractor = (request, response, next) => {
 
 const userExtractor = async (request, response, next) => {
   if (!request.token) {
-    return response.status(401).json({ error: 'token missimg' })
+    return response.status(401).json({ error: 'token missing' })
   }
 
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
--- part5/end-to-end/tests/blog_app.spec.js
+++ part5/end-to-end/tests/blog_app.spec.js
@@ -37,4 +37,54 @@ describe('Blog app', () => {
       await expect(page.getByText('wrong credentials')).toBeVisible()
     })
   })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('textbox').first().fill('newTestUser')
+      await page.getByRole('textbox').last().fill('123456')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('New User logged in')).toBeVisible()
+    })
+    
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'create new blog' }).click()
+      await expect(page.getByRole('heading', { name: 'create new' })).toBeVisible()
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill('newBlog')
+      await textboxes[1].fill('H.G. Wells')
+      await textboxes[2].fill('fake-url')
+      await page.getByRole('button', { name: 'create' }).click()
+      await expect(page.locator('.blogSummary').getByText('newBlog H.G. Wells')).toBeVisible()
+    })
+
+    test('a new blog can be liked', async ({ page }) => {
+      await page.getByRole('button', { name: 'create new blog' }).click()
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill('newBlog')
+      await textboxes[1].fill('H.G. Wells')
+      await textboxes[2].fill('fake-url')
+      await page.getByRole('button', { name: 'create' }).click()
+      
+      await page.getByRole('button', { name: 'view' }).click()
+      await page.getByRole('button', { name: 'like' }).click()
+      await expect(page.getByText('likes 1')).toBeVisible()
+    })
+
+    test('a new blog can be deleted', async ({ page }) => {
+      await page.getByRole('button', { name: 'create new blog' }).click()
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill('anothernewBlog')
+      await textboxes[1].fill('newAuthor')
+      await textboxes[2].fill('fake-url2')
+      await page.getByRole('button', { name: 'create' }).click()
+      
+      await expect(page.locator('.blogSummary').getByText('anothernewBlog newAuthor')).toBeVisible()
+      await page.getByRole('button', { name: 'view' }).click()
+      await expect(page.getByText('fake-url2')).toBeVisible()
+      page.once('dialog', dialog => dialog.accept())
+      await page.locator('.blogDetail').getByRole('button', { name: 'delete' }).click()
+      await expect(page.locator('.blogSummary').getByText('newBlog H.G. Wells')).not.toBeVisible()
+    })
+  })
+  
 })
